539cd6755736f2815a1426851aabd042eff45f4c
hassan-nahid <scientisthassannahid@gmail.com>
2025-07-04 19:52:01 +0600

    backed api added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api", 
+    baseUrl: "https://library-management-server-xi.vercel.app/api", 
   }),
   tagTypes: ["Book", "Borrow"],
   endpoints: () => ({}),
